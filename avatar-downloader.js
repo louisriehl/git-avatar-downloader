7389e46b25ca29034c69046d779acdce24cc1d32
Louis Riehl <louisriehl@gmail.com>
2018-10-01 18:38:31 +0000

    can make request through js, but we need a header to complete it

--- avatar-downloader.js
+++ avatar-downloader.js
@@ -3,7 +3,12 @@ var request = require('request');
 console.log('Welcome to the GitHub avatar downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+
+  request(url, (err, res, body) => {
+    cb(err, body);
+  });
+
 }
 
 getRepoContributors('jquery', 'jquery', (err, result) => {
