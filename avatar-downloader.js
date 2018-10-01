ee34f32668ab860e41456c9dc1aad296d1de31fb
Louis Riehl <louisriehl@gmail.com>
2018-10-01 18:33:14 +0000

    added hardcode test for core function

--- avatar-downloader.js
+++ avatar-downloader.js
@@ -1,3 +1,12 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub avatar downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+}
+
+getRepoContributors('jquery', 'jquery', (err, result) => {
+  console.log("Error:", err);
+  console.log("Result:", result);
+});
