21c97e4ee0edec7b046ee36a039b60df4556e801
Louis Riehl <louisriehl@gmail.com>
2018-10-01 21:41:15 +0000

    added error catching and messages

--- avatar-downloader.js
+++ avatar-downloader.js
@@ -1,20 +1,41 @@
 require('dotenv').config();
 var request = require('request');
 var fs = require('fs');
-// var authToken = require('./secrets');
-var myToken = 'token ' + process.env.AUTH_TOKEN;
+var chalk = require('chalk');
+var errorMessage = chalk.bold.red.bgYellow;
+var working = chalk.bold.inverse;
+
+
+
 
 // Take command line input
 var args = process.argv.slice(2);
+if(args.length !== 2 && args.length !== 0){
+  console.log(errorMessage('Incorrect number of arguments! Provide repository owner and name!'));
+  process.exit();
+}
+
 var owner = args[0];
 var repository = args[1];
 
 if(!owner || !repository) {
-  throw('No arguments given, please provide the repository owner and name');
+  console.log(errorMessage('No arguments given! Provide repository owner and name!'));
+  process.exit();
 }
 
+if(!fs.existsSync('./.env')) {
+  console.log(errorMessage('.env file does not exist to read AUTH_TOKEN!'));
+  process.exit();
+}
+if(!process.env.AUTH_TOKEN)
+{
+  console.log(errorMessage('AUTH_TOKEN in .env is null!'));
+  process.exit();
+}
 
-console.log('Welcome to the GitHub avatar downloader!');
+var myToken = 'token ' + process.env.AUTH_TOKEN;
+
+console.log(working('Welcome to the GitHub avatar downloader!'));
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
@@ -28,13 +49,22 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 
-  console.log('Getting avatars...');
+  console.log(working('Getting avatars...'));
   // Now we can make a request, taking our options and a function that returns the error, response, and body of our request
   request(options, (err, res, body) => {
 
     // The returned body is in a JSON string, we need to parse it to read it as an object
     var bodyObject = JSON.parse(body);
     var avatarURLS = [];
+    if (bodyObject.message)
+    {
+      if (bodyObject.message == "Not Found") {
+        console.log(errorMessage('Non-existant user or repository!'));
+      } else if (bodyObject.message == "Bad credentials") {
+        console.log(errorMessage('Invalid authorization token!'));
+      }
+      process.exit();
+    }
 
     // Push each of the avatar URLs from the object into an object containing the name and avatar url
     for (var n = 0; n < bodyObject.length; n++) {
@@ -45,7 +75,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
       avatarURLS.push(avatarObject);
     }
     // invoke callback function with our error (if we have one), and give it the array of avatar URLs
-    console.log('Downloading avatars...');
+    console.log(working('Downloading avatars...'));
     cb (err, avatarURLS);
   });
 
@@ -66,7 +96,7 @@ function downloadImageByURL(url, filePath) {
 
 getRepoContributors(owner, repository, (err, result) => {
   if(err) {
-    console.log("Error:", err);
+    console.log(errorMessage("Error:", err));
   }
   if(!fs.existsSync('./avatars/'))
   {
